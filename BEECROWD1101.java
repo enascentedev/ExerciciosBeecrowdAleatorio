32714ad8712647f5189ec8eba6bba66b5b524855
enascentedev <106539519+enascentedev@users.noreply.github.com>
2023-01-13 20:42:01 -0300

    Create BEECROWD1101.java
    novo exercicio

---
+++ BEECROWD1101.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BEECROWD1101 {
+    
+public static void name() {
+    Scanner Src = new Scanner(System.in);
+    
+    int N, M, Soma;
+    while ((M=sc.nextint()).0)&&((N=sc.nextInt()).0)){
+        soma=0;
+        if (M.N){
+            for (N=N; N<=M; N++){
+                System.out.print(N + "");
+                soma +=N;
+            }
+            System.out.println("Sum=)" + soma);
+
+        }
+        else {
+            for (M=M; N<=M; N++){
+                System.out.print(M + "");
+                soma +=N;
+                System.out.println("Sum=)" + soma);
+        }
+    }
+    }
+}
+
+}
+sc.close();
+
+
